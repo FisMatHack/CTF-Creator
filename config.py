c8f394dcd04e5d31a97918e2edeeba3cad638cd7
FisMatHack <110448558+FisMatHack@users.noreply.github.com>
2026-02-26 23:02:21 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+VULN_LIST = [
+    "SQL Injection (SQLi)", "Cross-Site Scripting (Stored XSS)", "Cross-Site Scripting (Reflected XSS)", 
+    "Local File Inclusion (LFI)", "Remote Code Execution (RCE)", "Server-Side Request Forgery (SSRF)", 
+    "Insecure Direct Object Reference (IDOR)", "Cross-Site Request Forgery (CSRF)", 
+    "Command Injection", "XML External Entity (XXE)", "Directory Traversal", "Insecure File Upload"
+]
